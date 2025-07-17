98fbefd0330743b92f4214ebe953e07b1a2e7dc4
BaeJHep <alexahepner@gmail.com>
2025-07-17 15:22:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,21 +9,21 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// REAL stream data from RapidAPI
+// REAL stream data from RapidAPI using Spotify Track IDs
 app.get('/api/streams', async (req, res) => {
   const headers = {
     'X-RapidAPI-Key': 'ef16e0854amsh9769731858997eep149822jsnc17a46c91c61',
     'X-RapidAPI-Host': 'spotify-track-streams-playback-count1.p.rapidapi.com'
   };
 
-  const url = 'https://spotify-track-streams-playback-count1.p.rapidapi.com/streams/isrc/';
-  const goldenISRC = 'QZ8BZ2513510';
-  const idolISRC = 'QZ8BZ2513512';
+  const url = 'https://spotify-track-streams-playback-count1.p.rapidapi.com/streams/track/';
+  const goldenTrackId = '1CPZ5BxNNd0n0nF4Orb9JS';
+  const idolTrackId = '1I37Zz2g3hk9eWxaNkj031';
 
   try {
     const [goldenRes, idolRes] = await Promise.all([
-      axios.get(url + goldenISRC, { headers }),
-      axios.get(url + idolISRC, { headers })
+      axios.get(url + goldenTrackId, { headers }),
+      axios.get(url + idolTrackId, { headers })
     ]);
 
     const goldenStreams = goldenRes.data?.data?.streamCount || 0;
@@ -48,3 +48,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
